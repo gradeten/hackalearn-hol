51685a9907486406ff8bd273d8eab40704ae73c5
gradeten <88281367+gradeten@users.noreply.github.com>
2021-08-13 00:29:25 +0900

    Update index.js

--- gatsby-app/src/pages/index.js
+++ gatsby-app/src/pages/index.js
@@ -144,7 +144,7 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations!!!
+        Wikipedia
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
         <span role="img" aria-label="Party popper emojis">
