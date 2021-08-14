e688cea3e21896b6a2f8bf6c49e4f9629a3e26b5
minji1289 <79117648+minji1289@users.noreply.github.com>
2021-08-14 22:12:13 +0900

    Update index.js

--- gatsby-app/src/pages/index.js
+++ gatsby-app/src/pages/index.js
@@ -122,17 +122,6 @@ const .navbar__menu li:hover {
     border-radius: 4px;
 }
 
-const .navbar__icons {
-    list-style: none;
-    color:white;
-    display: flex;
-    padding-left: 0;
-}
-
-const .navbar__icons li{
-    padding: 8px 12px;
-}
-
 const @media screen and (max-width: 768px) {
     .navbar{
         flex-direction: column;
@@ -238,6 +227,25 @@ const IndexPage = () => {
           😎
         </span>
       </p>
+// html 
+    <nav class="navbar">
+        <div class="navbar__logo">
+            <i class="fas fa-user-graduate"></i>
+            <a href="">TOGETHER</a>
+
+        </div>
+
+        <ul class="navbar__menu">
+            <li><a href="">MENU1</a></li>
+            <li><a href="">MENU2</a></li>
+            <li><a href=>MENU3</a></li>
+            <li><a href=>FAQ</a></li>
+            <li><a href="">Contact</a></li>
+
+        </ul>
+    
+    </nav>
+// html 
       <ul style={listStyles}>
         {posts.map(post => (
           <li key={post.id} style={{ ...listItemStyles}}>
